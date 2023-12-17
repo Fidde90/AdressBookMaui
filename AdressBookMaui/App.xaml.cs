baef3826e3766ea8f5b00d932616ea87378f3244
Fredrik <fredrik.t.bengtsson@hotmail.com>
2023-12-17 16:12:17 +0100

    Add project files.

---
+++ AdressBookMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdressBookMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AdressBookMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdressBookMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
