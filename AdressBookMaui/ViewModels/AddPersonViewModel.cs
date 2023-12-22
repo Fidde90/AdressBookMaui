e8c75fb8f5084ca726421d5ba0a24264c3101522
Fredrik <fredrik.t.bengtsson@hotmail.com>
2023-12-22 02:58:21 +0100

    Added pop up messeges

--- AdressBookMaui/ViewModels/AddPersonViewModel.cs
+++ AdressBookMaui/ViewModels/AddPersonViewModel.cs
@@ -2,7 +2,9 @@
 using AdressBook_Library.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls.Primitives;
 using System.Collections.ObjectModel;
+
 
 namespace AdressBookMaui.ViewModels
 {
@@ -20,20 +22,47 @@ namespace AdressBookMaui.ViewModels
         [ObservableProperty]
         private Person _registrationForm = new();
 
+        [ObservableProperty]
+        public bool added;
+
+        [ObservableProperty]
+        public bool fail;
+
         [RelayCommand]
-        void AddPerson()
+       void  AddPerson()
         {
             if (!string.IsNullOrWhiteSpace(RegistrationForm.Email))
             {
                 var succeed = _personService.AddPersonToList(RegistrationForm)!;
                 if (succeed)
                 {
-                    _personService.PersonListUpdated += (sender, e) =>
+                    _personService.PersonListUpdated += async (sender, e) =>
                     {
-                       _allPersonsViewModel.ObservablePersonList = new ObservableCollection<IPerson>(_personService.GetAllPersonsFromList());
+                        _allPersonsViewModel.ObservablePersonList = new ObservableCollection<IPerson>(_personService.GetAllPersonsFromList());
                     };
                     RegistrationForm = new();
+
+                    _ = TrueOrFalse(true);
                 }
+                else
+                    _ = TrueOrFalse(false);
+            }
+        }
+
+        async Task TrueOrFalse(bool trueOrFalse)
+        {
+            if (trueOrFalse)
+            {
+                Added = !Added;
+                await Task.Delay(2000);
+                Added = !Added;
+                RegistrationForm = new();
+            }
+            else
+            {
+                Fail = !Fail;
+                await Task.Delay(2000);
+                Fail = !Fail;
             }
         }
     }
